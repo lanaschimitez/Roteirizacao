9ec60499240b6d6250390923c324de27c22371e5
marcelodanelon <marcelodanelon11@gmail.com>
2020-11-11 20:07:28 -0300

    player

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float velocidade;
+    void Update()
+    {
+        transform.Translate(velocidade * Time.deltaTime, 0, 0);
+    }
+}

75be5a4af27fc9ffcfa31e5fcd4f0ec303b75cb3
marcelodanelon <marcelodanelon11@gmail.com>
2020-11-17 22:44:36 -0300

    puloRex

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public float velocidade;
+    public float forca;
+    Rigidbody playerBody;
+
+    private void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         transform.Translate(velocidade * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown("space"))
+        {
+            playerBody.AddForce(0, 50*forca, 0);
+        }
     }
 }

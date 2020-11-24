c8f957cd61f467206eabf88ea934a0afad43035a
lanaschimitez <lana.schimitez@gmail.com>
2020-11-24 08:47:23 -0300

    Menu&Consertos

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    public float speed;
+    public float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = transform.position.x;
+        x += speed * Time.deltaTime;
+
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+
+        if (x <= -11)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,19 +6,28 @@ public class player : MonoBehaviour
 {
     public float velocidade;
     public float forca;
+    bool pulo;
     Rigidbody playerBody;
 
     private void Start()
     {
+        pulo = false;
         playerBody = GetComponent<Rigidbody>();
     }
     void Update()
     {
         transform.Translate(velocidade * Time.deltaTime, 0, 0);
 
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") && pulo == false)
         {
-            playerBody.AddForce(0, 50*forca, 0);
+            pulo = true;
+            playerBody.AddForce(0, 50 * forca, 0);
+            Invoke("PermitePulo", 1f);
         }
+    }
+
+    void PermitePulo()
+    {
+        pulo = false;
     }
 }

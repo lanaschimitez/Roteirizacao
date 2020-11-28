cbc9e173c39216ffef5b9fb4256352374c565438
marcelodanelon <58780939+marcelodanelon@users.noreply.github.com>
2020-11-27 21:21:18 -0300

    TimeandMenu

---
+++ Assets/Scripts/Game/controllerGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controllerGame : MonoBehaviour
+{
+    public Text textTempo;
+    public float gameTimer;
+
+    private void Start()
+    {
+        gameTimer = 0;
+    }
+    void Update()
+    {
+        gameTimer += Time.deltaTime;
+
+        int seconds = (int)(gameTimer % 60);
+        int minutes = (int)(gameTimer / 60) % 60;
+        PlayerPrefs.SetFloat("time", gameTimer);
+
+        string timerString = string.Format("{0:00}:{1:00}", minutes, seconds);
+        textTempo.text = timerString;
+        }
+    }
+
---
+++ Assets/Scripts/Menu/controllerMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class controllerMenu : MonoBehaviour
+{
+    public Text textOptions;
+    public Text textCredits;
+    public Text textExit;
+
+    public Image imageOptions;
+    public Image imageCredits;
+    public Image imageExit;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void onOptions()
+    {
+        textOptions.fontSize = 38;
+        imageOptions.color = new Color32(245,134,52,255);
+    }
+    public void offOptions()
+    {
+        textOptions.fontSize = 32;
+        imageOptions.color = new Color32(0, 0, 0, 255);
+    }
+    public void onCredits()
+    {
+        textCredits.fontSize = 38;
+        imageCredits.color = new Color32(245, 134, 52, 255);
+    }
+    public void offCredits()
+    {
+        textCredits.fontSize = 32;
+        imageCredits.color = new Color32(0, 0, 0, 255);
+    }
+    public void onExit()
+    {
+        textExit.fontSize = 38;
+        imageExit.color = new Color32(245, 134, 52, 255);
+    }
+    public void offExit()
+    {
+        textExit.fontSize = 32;
+        imageExit.color = new Color32(0, 0, 0, 255);
+    }
+}

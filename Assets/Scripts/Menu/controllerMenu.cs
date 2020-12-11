2f749e518be1ae7439c3e82b38efb483f7faf054
Lana Eduarda Schimitez <lana.schimitez@gmail.com>
2020-12-10 22:32:24 -0300

    Att UI

--- Assets/Scripts/Menu/controllerMenu.cs
+++ Assets/Scripts/Menu/controllerMenu.cs
@@ -25,43 +25,43 @@ public class controllerMenu : MonoBehaviour
 
     public void onPlay()
     {
-        textPlay.fontSize = 38;
+        textPlay.fontSize = 58;
         imagePlay.color = new Color32(245, 134, 52, 255);
     }
     public void offPlay()
     {
-        textPlay.fontSize = 32;
+        textPlay.fontSize = 50;
         imagePlay.color = new Color32(0, 0, 0, 255);
     }
 
     public void onOptions()
     {
-        textOptions.fontSize = 38;
+        textOptions.fontSize = 58;
         imageOptions.color = new Color32(245,134,52,255);
     }
     public void offOptions()
     {
-        textOptions.fontSize = 32;
+        textOptions.fontSize = 50;
         imageOptions.color = new Color32(0, 0, 0, 255);
     }
     public void onCredits()
     {
-        textCredits.fontSize = 38;
+        textCredits.fontSize = 58;
         imageCredits.color = new Color32(245, 134, 52, 255);
     }
     public void offCredits()
     {
-        textCredits.fontSize = 32;
+        textCredits.fontSize = 50;
         imageCredits.color = new Color32(0, 0, 0, 255);
     }
     public void onExit()
     {
-        textExit.fontSize = 38;
+        textExit.fontSize = 58;
         imageExit.color = new Color32(245, 134, 52, 255);
     }
     public void offExit()
     {
-        textExit.fontSize = 32;
+        textExit.fontSize = 50;
         imageExit.color = new Color32(0, 0, 0, 255);
     }
 }
